75886a42cf212614c8d1dc098a2da44d2a68a017
robin ucar <ucarmehmet.ch@gmail.com>
2022-01-04 16:02:08 +0000

    sinatra reloader and some paths

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require 'sinatra'
-get '/' do
-    'Hello World'
-end
+require "sinatra/reloader" if development?
+
+
+
+    get '/' do
+        'Hello World'
+    end
+
+    get '/secret' do
+        'This is a secret between me and you!'
+    end
+
+    get '/robin' do
+        'Hello I am Robin...'
+    end
+
+    get '/project' do
+        'This is a first web app project using sinatra!'
+    end
